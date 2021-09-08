c77336ba07e216c79a5e35d2b474549f15b52ffc
Dawie van Lill <DawievLill@users.noreply.github.com>
2021-09-08 10:12:01 +0200

    Commenting on my code

--- replications/3_numbers.jl
+++ replications/3_numbers.jl
@@ -24,4 +24,4 @@ p1 = compound_interest(0.04, 365, 1000, 10)
 p2 = compound_interest(0.04, 365 * 24, 10e9, 100)
 p3 = compound_interest(Decimal(0.04), 365 * 24, Decimal(10e9), 100)
 
-diff = number(p3) - p2
+diff = number(p3) - p2 # This takes the difference
