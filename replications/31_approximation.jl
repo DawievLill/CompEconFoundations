0070e7ff7beba432cbcd1ceb1c916b6aa1e9328a
Dawie <dvanlill@sun.ac.za>
2021-10-31 19:58:57 +0200

    Basics of interpolation

--- replications/31_approximation.jl
+++ replications/31_approximation.jl
@@ -14,5 +14,31 @@ Function approximation
 
 # Spline interpolation
 
+using Distributions
+using Interpolations
+using Plots
 using Random
 
+x = sort(rand(Uniform(-5, 10), 12)) # random number sequence
+xr = LinRange(-5, 10, 12) # regular grid on [-5, 10]
+
+func = x -> @. exp(-x / 4) * sin(x) + 1 / (1 + x ^ 2) # Function to interpolate
+
+function plot_help(ifunc, fdata = func(x), f = func)
+
+    xd = LinRange(-5, 10, 1000)
+
+    scatter(fdata)
+    plot!(xd, f(xd))
+
+    #xdi = xd[xd .>= fdata[1][1] .&& xd .<= fdata[1][end]]
+
+    #if ifunc
+    #    plot!(xdi , ifunc(xdi))
+    #end
+
+end
+
+plot_help(nothing)
+
+fi = LinearInterpolation(x, func(x)) # returns interpolation function
